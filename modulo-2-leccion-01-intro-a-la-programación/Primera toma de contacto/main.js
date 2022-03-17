d0f750fe6f6290bcad6c41e716fe818a94b5a3f4
Veronica isla <veroislagleaniz@gmail.com>
2022-03-17 16:13:43 +0100

    first message with JS

---
+++ modulo-2-leccion-01-intro-a-la-programación/Primera toma de contacto/main.js
@@ -0,0 +1,4 @@
+"use strict";
+document.querySelector("h1").innerHTML = "¡Hola mundo!";
+
+document.querySelector("h2").innerHTML="Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente";
