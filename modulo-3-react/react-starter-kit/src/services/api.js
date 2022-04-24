b45f290aa410d32b4f22ffdbf1b4c233381decab
Veronica isla <veroislagleaniz@gmail.com>
2022-04-24 11:41:12 +0200

    created folders

---
+++ modulo-3-react/react-starter-kit/src/services/api.js
@@ -0,0 +1,20 @@
+// // Fichero src/services/api.js
+
+// const callToApi = () => {
+//   // Llamamos a la API
+//   return fetch('') //
+//     .then((response) => response.json())
+//     .then((response) => {
+//       // Cuando responde la API podemos limpiar los datos aquí, no es obligatorio
+//       const result = {
+//         name: response.name,
+//         birthYear: response.birth_year,
+//         height: response.height,
+//         mass: response.mass,
+//         eyeColor: response.eye_color,
+//       };
+//       return result;
+//     });
+// };
+
+// export default callToApi;
